17ea7f1eb0229abe3e3ea0144c25f0f67abf062d
gouzun <gouzun1@gmail.com>
2023-08-16 23:33:55 +0800

    16-8-23

--- client/src/screen/Projects.jsx
+++ client/src/screen/Projects.jsx
@@ -19,53 +19,104 @@ const Projects = () => {
   //   pLink: '',
   // });
 
-  const [resList, setResList] = useState([]);
+  const [resList, setResList] = useState([
+    {
+      pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1685426058/portfolio/defect_ubheql.png',
+      pAlt: 'Defect Inspection Web App',
+      pTitle: 'Defect Inspection Web App',
+      pDesc: 'A defect inspection web app is a software application designed to facilitate the inspection of products or systems for defects or issues. The app typically involves the use of a mobile device, such as a smartphone or tablet, that is equipped with a camera and a web-based interface for data input and analysis. The inspector uses the app to capture images of the product or system being inspected, and then records any defects or issues using a set of pre-defined criteria or checklists. ',
+      pLink: 'https://inspectmy.netlify.app',
+    },
+    {
+      pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1683093002/portfolio/aisummarizer_wp23js.png',
+      pAlt: 'AI Articles Summarizer',
+      pTitle: 'AI Articles Summarizer Web App',
+      pDesc: 'An AI articles summarizer web app is a software application that automatically generates a brief summary of a longer article or document. The app typically uses machine learning algorithms to analyze the text of the article and identify key themes, concepts, and ideas. It then condenses the information into a shorter, more digestible format that can be quickly read and understood.',
+      pLink: 'https://hcaisummarizer.netlify.app',
+    },
+    {
+      pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1683092739/portfolio/dalle_yhati1.png',
+      pAlt: 'AI Art Generator',
+      pTitle: 'AI Art Generator Web App',
+      pDesc: 'An AI art generator web app is a software application that uses machine learning algorithms to generate unique and original art pieces. The app typically utilizes a neural network that has been trained on a large dataset of artworks, which allows it to learn and mimic various styles and techniques. The user can input specific parameters such as the desired art style, colors, and patterns, and the app will generate a custom art piece based on those inputs.The primary goal of an AI art generator web app is to provide users with a new and innovative way to create art that is both unique and personalized',
+      pLink: 'https://aiartgenerator.whtech.me',
+    },
+    {
+      pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1685377525/portfolio/scheduler_aghigs.png',
+      pAlt: 'Staff Scheduler',
+      pTitle: 'Staff Scheduler Web App',
+      pDesc: 'Introducing Scheduler, a cutting-edge staff scheduler arrangement tool designed to streamline and optimize workforce management for businesses of all sizes. With its intuitive interface and advanced features, Scheduler revolutionizes the way companies organize their staff schedules, ensuring optimal efficiency and productivity. Save time, reduce conflicts, and enhance employee satisfaction with Scheduler, your trusted partner in effective staff scheduling and resource allocation.',
+      pLink: 'http://scheduler.whtech.me',
+    },
+    {
+      pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1685977425/portfolio/rn_sbsxhf.png',
+      pAlt: 'INSPECTMY(ANDROID VERSION)',
+      pTitle: 'INSPECTMY(ANDROID VERSION)',
+      pDesc: 'Introducing the Defect Inspection Tool for Android, a revolutionary solution that enables offline defect inspection. Combining advanced machine vision algorithms with the power of Android devices, this tool allows seamless capture, analysis, and identification of defects using the device camera. With its offline capability, inspectors can perform inspections without relying on network connectivity, making it ideal for remote areas or situations with limited internet access.',
+      pLink: '',
+    },
+    {
+      pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1685978100/portfolio/INSPECTMY_REPORT_c0wvzg.png',
+      pAlt: 'INSPECTMY REPORTING WEB APP',
+      pTitle: 'INSPECTMY REPORTING WEB APP',
+      pDesc: 'This is reporting tool for the Android Version INSPECTMY, a powerful web application that seamlessly generates comprehensive reports. This tool complements the Android version INSPECTMY by providing a user-friendly interface for users to view and analyze inspection data. Users can access the web app online and generate detailed reports in PDF format, making it convenient for sharing and archiving. ',
+      pLink: 'https://inspectmyreport.whtech.me',
+    },
+      {
+        pImg: 'https://res.cloudinary.com/drpsfwq3y/image/upload/v1683093307/portfolio/hc_ne2kbi.png',
+        pAlt: 'Hoo Construction Sdn Bhd',
+        pTitle: 'Company Landing Page',
+        pDesc: 'The function of a company landing page is to provide visitors with a brief overview of the company, its products or services, and its mission and values. It serves as the first point of contact between the company and potential customers or clients, and should be designed to make a positive first impression. A well-designed landing page should be visually appealing, easy to navigate, and provide visitors with the information they need to make informed decisions about whether to engage with the company further.',
+        pLink: 'https://hooconstruction.netlify.app',
+      },
+      
+  ]);
   const [isLoading, setIsLoading] = useState('');
-  useEffect(() => {
-    fetchPosts();
-  }, [])
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, [])
 
-  const fetchPosts = async () => {
+  // const fetchPosts = async () => {
 
-    try {
-      setIsLoading(false);
-      let responseClone;
-      const response = await fetch('https://portfolio-8q1p.onrender.com/api/v1/projectlist', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
+  //   try {
+  //     setIsLoading(false);
+  
+  //     const response = await fetch('https://portfolio-8q1p.onrender.com/api/v1/projectlist', {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json',
 
-        },
-        // }).then(function (response) {
-        //   responseClone = response.clone(); // 2
-        //   return response.json();
-        // })
-        //   .then(function (data) {
-        //     // Do something with data
-        //   }, function (rejectionReason) { // 3
-        //     console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
-        //     responseClone.text() // 5
-        //       .then(function (bodyText) {
-        //         console.log('Received the following instead of valid JSON:', bodyText); // 6
-        //       });
-      });
+  //       },
+  //       // }).then(function (response) {
+  //       //   responseClone = response.clone(); // 2
+  //       //   return response.json();
+  //       // })
+  //       //   .then(function (data) {
+  //       //     // Do something with data
+  //       //   }, function (rejectionReason) { // 3
+  //       //     console.log('Error parsing JSON from response:', rejectionReason, responseClone); // 4
+  //       //     responseClone.text() // 5
+  //       //       .then(function (bodyText) {
+  //       //         console.log('Received the following instead of valid JSON:', bodyText); // 6
+  //       //       });
+  //     });
 
 
 
-      if (response.ok) {
-        const result = await response.json();
-        setResList(result.data.reverse());
-      }
-    } catch (err) {
-      alert(err);
-    } finally {
-      setIsLoading(true);
-    }
-  };
+  //     if (response.ok) {
+  //       const result = await response.json();
+  //       setResList(result.data.reverse());
+  //     }
+  //   } catch (err) {
+  //     alert(err);
+  //   } finally {
+  //     setIsLoading(true);
+  //   }
+  // };
 
   return (<>
     <div className="flex flex-wrap justify-center items-center  gap-20 pt-10">
-      {isLoading ? resList.map((projectinfo, index) => {
+      {resList.map((projectinfo, index) => {
         return (
           <Card className="w-96" key={index}>
             <CardHeader color="blue" className="relative h-64 md:h-72 ">
@@ -88,7 +139,7 @@ const Projects = () => {
               </Typography>
             </CardBody>
           </Card>)
-      }) : <Loader />}
+      }) }
     </div>
 
   </>)
